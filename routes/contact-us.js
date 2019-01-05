6730f930d392bd355f3b756edd68e469eb3960a0
sumit <sumitkumarpoint@gmail.com>
2019-01-05 17:26:56 +0530

    after contact us and about us

---
+++ routes/contact-us.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('contact-us', {page:'Contact Us', menuId:'home'});
+});
+
+module.exports = router;
