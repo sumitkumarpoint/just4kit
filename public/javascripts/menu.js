cec5ffd6785797ac39b7faf704877f310b006054
sumit <sumitkumarpoint@gmail.com>
2019-01-09 17:06:58 +0530

    after loader

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
     var element = $('meta[name="active-menu"]').attr('content');
     $('#' + element).addClass('active');
 });
+function showLoader() {
+document.getElementById('loader').style.display='block'
+}
+$('.menu').click(function () {
+    showLoader();
+    return;
+});
