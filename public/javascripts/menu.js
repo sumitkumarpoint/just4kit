5f2e4f42949d87659b6b46c67eaaaffee12b018c
sumit <sumitkumarpoint@gmail.com>
2019-01-11 19:13:07 +0530

    after fixed flash

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -10,8 +10,9 @@ $('.menu').click(function () {
     showLoader();
     return;
 });
-var l=($( window ).width()-$('.flash span').width())/2;
-$('.flash span').css({'position': 'fixed', 'top': '5%','left':l+'px'});
+var padding= 30;
+var l=($( window ).width()-$('.flash span').width())/2-padding;
+$('.flash span').css({'position': 'fixed', 'top': '5%','left':l+'px', 'padding': '5px '+padding+'px'});
 setTimeout(function () {
     $('.flash').css({'display':'none'});
 },5000);
